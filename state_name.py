2f8f3e312f47adb1a721b019804d23d0b5f0d7f9
limengyuan <mengyuan.li@my.jcu.edu.au>
2018-12-04 11:29:02 +0800

    enter name short state

--- state_name.py
+++ state_name.py
@@ -7,7 +7,7 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
-# print(STATE_NAMES)
+
 
 state = input("Enter short state: ").upper()
 while state != "":
